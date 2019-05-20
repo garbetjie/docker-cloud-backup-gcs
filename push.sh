f8d4f31a73fb55ab10e000a644ef10cf376672a7
Geoff Garbers <geoff@garbers.co.za>
2019-05-20 21:52:27 +0200

    Update push script.

--- push.sh
+++ push.sh
@@ -3,14 +3,22 @@
 set -e
 
 # Extract the current version.
-version="$(grep FROM Dockerfile | cut -f2 -d: | cut -f1 -d-)"
+version="$(git describe --always)"
+
+# Confirm version.
+printf "About to push version \`${version}\`. Is this correct? [Y/n] "
+read confirm
+
+if [[ "$confirm" = "N" ]] || [[ "$confirm" = "n" ]]; then
+	exit 0
+fi
 
 # Build latest Docker image.
-docker build -t garbetjie/backup-gcs:latest .
+docker build -t garbetjie/cloud-backup:gcs-latest .
 
 # Build the versioned image.
-docker tag garbetjie/backup-gcs:latest garbetjie/backup-gcs:$version
+docker tag garbetjie/cloud-backup:gcs-latest garbetjie/cloud-backup:gcs-$version
 
 # Push images.
-docker push garbetjie/backup-gcs:latest
-docker push garbetjie/backup-gcs:$version
+docker push garbetjie/cloud-backup:gcs-latest
+docker push garbetjie/cloud-backup:gcs-$version
